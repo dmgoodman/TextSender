6ea1f58c54356bcb1f3b57f7197c3e48806116ee
dmgoodman <43937598+dmgoodman@users.noreply.github.com>
2020-11-22 21:45:56 -0500

    Refactoring

--- textsender.py
+++ textsender.py
@@ -1,9 +1,11 @@
 import fbchat
+import threading
 from get_credentials import get_credentials
 
 MYSELF = 100015764883289 # User id for Daniel Goodman
 RECEIVER = MYSELF # User to receive messages
 SENDER_EMAIL, SENDER_PASSWORD = get_credentials()
+SHEET = ""
 
 def send(message):
 	client = fbchat.Client(SENDER_EMAIL, SENDER_PASSWORD)
@@ -13,9 +15,30 @@ def send(message):
 	else:
 		print("Oh my lord, something went wrong.")
 
-def check_sheet():
-	return "What in the world is the date?"
+def handle_sheet(sheet):
+	''' Return list of messages and delete corresponding rows in sheet if sheet not empty. Else return []. '''
+
+	# Access sheet
+
+	messages = []
+	# Populate messages and delete from sheet
+
+	return messages
+
+def loop():
+	threading.Timer(5.0, loop).start()
+
+	messages = handle_sheet(SHEET)
+	for message in messages:
+		message = message.split("///", 3)
+	
+		message_string = message[1] if len(message[2]) == 0 else message[2]
+		message_string += ": " + message[3]
+		send(message_string) 
+
+	print("looped")
+
 
 if __name__ == "__main__":
-	message = check_sheet()
-	send(message)
+	loop()
+	
